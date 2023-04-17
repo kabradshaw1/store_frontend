fb05422b8cd25a7cd915308a951f2f5c2892831c
kabradshaw1 <itllbok82@gmail.com>
2023-04-17 09:34:15 -0400

    added most of the other stores backend

---
+++ app/store/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import Item, Order, OrderedItem, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+  items = serializers.StringRelatedField(many=True)
+
+  class Meta:
+    model = Category
+    fields = ('id', 'name', 'items')
+
+class ItemSerializer(serializers.ModelSerializer):
+  category = serializers.StringRelatedField()
+
+  class Meta:
+    model = Item
+    fields = ('id', 'name', 'price', 'image', 'description', 'quantity', 'created', 'updated', 'category', 'slug')
+
+class OrderedItemSerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model = OrderedItem
+    fields = ('item', 'cartQty', 'id', 'price')
+
+class OrderSerializer(serializers.ModelSerializer):
+  ordered_items = OrderedItemSerializer(many=True)
+
+
+  class Meta:
+    model = Order
+    fields = ('id', 'created', 'updated', 'price', 'ordered_items')
+
+  def create(self, validated_data):
+    ordered_items_data = validated_data.pop('ordered_items')
+    order = Order.objects.create(**validated_data)
+
+    for ordered_item_data in ordered_items_data:
+      OrderedItem.objects.create(order=order, **ordered_item_data)
+    return order
---
+++ app/user/views.py
@@ -0,0 +1,82 @@
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import filters, viewsets, status
+from rest_framework.viewsets import ViewSet
+from rest_framework.permissions import AllowAny
+
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+
+from user.models import User
+from user.serializers import UserSerializer, RegisterSerializer, LoginSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    http_method_names = ['get']
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['updated']
+    ordering = ['-updated']
+
+    def get_queryset(self):
+        return User.objects.all()
+
+    def get_object(self):
+        lookup_field_value = self.kwargs[self.lookup_field]
+
+        obj = User.objects.get(id=lookup_field_value)
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+class LoginViewSet(ViewSet):
+    serializer_class = LoginSerializer
+    permission_classes = (AllowAny,)
+    http_method_names = ['post']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+class RefreshViewSet(viewsets.ViewSet, TokenRefreshView):
+    permission_classes = (AllowAny,)
+    http_method_names = ['post']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+class RegistrationViewSet(ViewSet):
+    serializer_class = RegisterSerializer
+    permission_classes = (AllowAny,)
+    http_method_names = ['post']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        res = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
+
+        return Response({
+            "user": serializer.data,
+            "refresh": res["refresh"],
+            "token": res["access"]
+        }, status=status.HTTP_201_CREATED)
